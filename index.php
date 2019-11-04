b41725d19b47d26660c2e9433882e0dab44009a0
Justin <justin-z@live.nl>
2019-11-04 13:58:45 +0100

    opdracht 3, wist niet dat ik per opdracht moest verzenden

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
  */
 
 
+
+$boodschap = ["aardappel", "aardbeien", "3 pakken melk", "yogurt"];
+//echo $boodschap[1], $boodschap[2], $boodschap[3], $boodschap[4];
+
 ?>
 
-<h1>Test 2</h1>
+<div>
+    <ul>
+        <li>
+           <?php echo $boodschap[0]?>
+        </li>
+        <li>
+            <?php echo $boodschap[1]?>
+        </li>
+        <li>
+            <?php echo $boodschap[2]?>
+        </li>
+        <li>
+            <?php echo $boodschap[3]?>
+        </li>
+    </ul>
+</div>
