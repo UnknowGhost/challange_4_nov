e2e3ea44c7c0365f00c3521a012a3af4135f7a9d
Justin <justin-z@live.nl>
2019-11-13 10:36:53 +0100

    opdracht 3, wist niet dat ik per opdracht moest verzenden

---
+++ opdr.4.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Justin
+ * Date: 4-11-2019
+ * Time: 14:01
+ */
+?>
+
+
+
+    <div>
+        yeet
+        <div>Voeg toe :</div>
+        <input type="text" name="txt">
+        <input type="submit" name="select" value="select" onclick="select()">
+    </div>
+
+<?php
+function select(){
+    echo "The select function is called.";
+}
+
+?>
